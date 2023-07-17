2b0b7df1f5f3f256393e4e081a1adcaf65326777
Henry.Wang <Henry.Wang@fanruan.com>
2023-07-17 17:45:30 +0800

    提交代码

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,8 @@ interface User {
 }
 
 export function getAdults(users: (User | null)[]): User[] {
-  return users.filter(Boolean).filter((user) => user.age >= 18);
+  const notNullPredicate = <T>(value: T | null): value is T => {
+    return value !== null;
+  };
+  return users.filter(notNullPredicate).filter((user) => user.age >= 18);
 }
